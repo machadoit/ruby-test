ee8aafe9fcfb631bc5004a98e75fd41b8f299a0c
Joao Machado <machado@codacy.com>
2016-06-14 18:00:18 +0100

    Add scala issue

--- testScala.scala
+++ testScala.scala
@@ -18,5 +18,216 @@ object myObject {
       case bla if bla > 2 => "yey"
       case blaa if blaa > 1 => "nop"
     }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+    
+        Try(1 / 0) match {
+      case Success(content) => "yey"
+      case Failure(fail) => "nop"
+    }
+
+    List(1, 2, 3).collectFirst {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
+
+    //BUM!!!
+    List(1, 2, 3).map {
+      case bla if bla > 2 => "yey"
+      case blaa if blaa > 1 => "nop"
+    }
   }
 }
